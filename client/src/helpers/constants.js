aa54e47e2088c1fb873e30beea2cec086b0a76fb
Kyle Zhou <kzkyle2000@gmail.com>
2024-05-13 14:49:21 +1200

    client node env

--- client/src/helpers/constants.js
+++ client/src/helpers/constants.js
@@ -1,2 +1,2 @@
 export const APIROOT =
-  process.env.NODE_ENV === 'development' ? 'http://localhost:5000' : 'https://api.zephyrapp.nz';
+  process.env.NODE_ENV === 'production' ? 'https://api.zephyrapp.nz' : 'http://localhost:5000';
