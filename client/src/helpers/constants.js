5971f189431efaf84f480321b4677aa855f92825
Kyle Zhou <kzkyle2000@gmail.com>
2024-05-17 10:23:05 +1200

    client cams - use fs url

--- client/src/helpers/constants.js
+++ client/src/helpers/constants.js
@@ -1,2 +1,2 @@
 export const APIROOT =
-  process.env.NODE_ENV === 'production' ? 'https://api.zephyrapp.nz' : 'http://localhost:5000';
+  process.env.NODE_ENV === 'production' ? 'https://fs.zephyrapp.nz' : 'http://localhost:5000';
